4cd1a012dfe7d84910798583a06fff575d8ec80a
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-25 00:44:08 +0600

    shoes

---
+++ src/component/Cosmetics/Cosmetics.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { add } from "../../utilities/calculate";
+// import add from "../../utilities/calculate";
+
+const Cosmetics = () => {
+  const first = 55;
+  const second = 66;
+  const total = add(first, second);
+  return (
+    <div>
+      <h1>Welcome to my cosmetic Store</h1>
+      <p>total: {total}</p>
+    </div>
+  );
+};
+
+export default Cosmetics;
---
+++ src/utilities/calculate.js
@@ -0,0 +1,10 @@
+const add = (first, second) => {
+  return first + second;
+};
+
+// export default add;
+
+const multiply = (first, second) => {
+  return first * second;
+};
+export { add, multiply };
