2d413a43da29e7f0d78d911c0b7140d935805a2f
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-25 03:05:53 +0600

    shoes

--- src/component/Cosmetics/Cosmetics.js
+++ src/component/Cosmetics/Cosmetics.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { add } from "../../utilities/calculate";
-// import add from "../../utilities/calculate";
+import React, { useEffect, useState } from "react";
+import Cosmetic from "../cosmetic/Cosmetic";
 
 const Cosmetics = () => {
-  const first = 55;
-  const second = 66;
-  const total = add(first, second);
+  const [cosmetics, setCosmetics] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCosmetics(data));
+  }, []);
   return (
     <div>
       <h1>Welcome to my cosmetic Store</h1>
-      <p>total: {total}</p>
+      {cosmetics.map((cosmetic) => (
+        <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>
+      ))}
     </div>
   );
 };
 
 export default Cosmetics;
+
+// const first = 55;
+//   const second = 66;
+//   const total = add(first, second);
